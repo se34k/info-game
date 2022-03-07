4b7fb35bea7e2099b8e891a7ad374eaf391940c0
EveryLimit <mihairadupredoi@yahoo.com>
2022-03-07 19:45:08 +0100

    libGDX go ling schiling

--- desktop/src/com/infolk/game/desktop/DesktopLauncher.java
+++ desktop/src/com/infolk/game/desktop/DesktopLauncher.java
@@ -4,8 +4,11 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.infolk.game.App;
 
+/**
+ * @author Mihai
+ */
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Scheiss Zelda like game v.069";
 		config.width = 1920;
