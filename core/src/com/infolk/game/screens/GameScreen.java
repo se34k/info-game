c2c45786f4e50445834fce6d4e7c3b533eb840f2
EveryLimit <mihairadupredoi@yahoo.com>
2022-03-08 18:29:19 +0100

    minor clean up

--- core/src/com/infolk/game/screens/GameScreen.java
+++ core/src/com/infolk/game/screens/GameScreen.java
@@ -1,6 +1,5 @@
 package com.infolk.game.screens;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -13,44 +12,37 @@ public class GameScreen implements Screen {
 
 	@Override
 	public void show() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void render(float delta) {
-		// TODO Auto-generated method stub
 		ScreenUtils.clear(Color.BLACK);
 
 	}
 
 	@Override
 	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void pause() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void resume() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void hide() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
 
 	}
 
