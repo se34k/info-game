8c1119ced2bf25117a2c2cd69256954f4ed14f48
Flachwixxer <diefrizler@hotmail.com>
2022-03-20 14:15:27 +0100

    dad

--- core/src/com/infolk/game/combat/Assi.java
+++ core/src/com/infolk/game/combat/Assi.java
@@ -1,10 +1,12 @@
 package com.infolk.game.combat;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 public class Assi extends Enemy{
   
   public Assi(float x, float y){
-    super("Assi", 15, new Sprite(new Texture(Gdx.files.internal("Assi.png")), x, y, 20f);
+    super("Assi", 15, new Sprite(new Texture(Gdx.files.internal("Assi.png"))), x, y, 20f);
   }
    
   public void verfolgen(Entity entity){
--- core/src/com/infolk/game/combat/Ratte.java
+++ core/src/com/infolk/game/combat/Ratte.java
@@ -1,10 +1,12 @@
 package com.infolk.game.combat;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 public class Ratte extends Enemy{
   
   public Ratte(float x, float y){
-    super("Ratte", 5, new Sprite(new Texture(Gdx.files.internal("Ratte.png")), x, y, 30f);
+    super("Ratte", 5, new Sprite(new Texture(Gdx.files.internal("Ratte.png"))), x, y, 30f);
   }
    
   public void verfolgen(Entity entity){
