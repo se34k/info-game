2b36879534c63a6557248cc4be5b13e99a6f62b0
se34k <papagei_email@web.de>
2022-03-20 16:21:30 +0100

    performance improvements

--- core/src/com/infolk/game/combat/Bauarbeiter.java
+++ core/src/com/infolk/game/combat/Bauarbeiter.java
@@ -5,11 +5,11 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
 public class Bauarbeiter extends Enemy {
-  public Bauarbeiter(float x, float y){
-    super("Bauarbeiter", 40, new Sprite(new Texture(Gdx.files.internal("Bauarbeiter.png"))), x, y, 10f);
-  }
-   
-  public void verfolgen(Entity entity){
-    super.track(entity);
-  }
+    public Bauarbeiter(float x, float y) {
+        super("Bauarbeiter", 40, new Sprite(new Texture(Gdx.files.internal("Bauarbeiter.png"))), x, y, 10f);
+    }
+    
+    public void verfolgen(Entity entity) {
+        super.track(entity);
+    }
 }
