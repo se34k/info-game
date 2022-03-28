179b9c6c897176e953a5c8be1c77d5f90395c565
VladGamex <v.kimbar@web.de>
2022-03-28 23:32:52 +0200

    Merge branch 'main' of https://github.com/EveryLimit/info-game

--- core/src/com/infolk/game/combat/Playable.java
+++ core/src/com/infolk/game/combat/Playable.java
@@ -1,13 +1,26 @@
 package com.infolk.game.combat;
 
+import java.util.ArrayList;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
 public class Playable extends Entity {
     private int dashFactor;
     private float energy;
 
+
+    // Not working
+        private int numberOfBooks;
+        private ArrayList<Sprite> books;
+    //
     public Playable(String name, int hp, Sprite sprite) {
         this(name, hp, sprite, 0, 0);
+
+        numberOfBooks = 5;
+        books = new ArrayList<>();
+        addBooks();
     }
 
     public Playable(String name, int hp, Sprite sprite, float x, float y) {
@@ -21,6 +34,9 @@ public class Playable extends Entity {
         super.displayBar = false;
         setPosition(20, 20);
         setSpeed(50);
+        numberOfBooks = 5;
+        books = new ArrayList<>();   
+        addBooks();
     }
 
     @Override
@@ -48,4 +64,25 @@ public class Playable extends Entity {
             dashFactor = 1;
         }
     }
+
+    // Not working
+        public void addBooks() {
+            for(int i = 0; i < numberOfBooks; i++) {
+                books.add(new Sprite(new Texture(Gdx.files.internal("sprites/Buch.png"))));
+            }
+        }
+
+        public void throwBook(ArrayList<Entity> entities) {
+            if(books.size() > 0) {
+            int deg = (int) getVelocity().angleDeg();
+            float pX = getX();
+            float pY = getY();
+
+            if(deg == 0) {
+
+            }
+            
+            }
+        }
+    //
 }
